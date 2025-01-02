d7c62abd98aa94c848833d652f1603fd662ac2aa
ro35ert <trebrorobertsbob@gmail.com>
2025-01-02 15:09:09 +0200

    Make Header title clickable to navigate to Home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FiBell, FiMessageSquare, FiUser } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const nav = useNavigate();
     return (
         <div className="bg-blue-600 text-white p-4 flex items-center justify-between">
-            <h1 className="text-xl font-bold">LearningHub</h1>
+            <h1 onClick={()=>{nav("/home")}}  className="text-xl font-bold cursor-pointer">LearningHub</h1>
             <div className="flex space-x-4">
                 <button className="relative">
                     <FiBell size={24} />
